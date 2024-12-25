4167b0bec8e69af7e12ce0065adda55b2170374f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-12-25 15:03:08 +0200

    Improved item events

--- Apps.Monday/Webhooks/ItemWebhookList.cs
+++ Apps.Monday/Webhooks/ItemWebhookList.cs
@@ -1,21 +1,38 @@
-﻿using Apps.Monday.Models.Identifiers;
+﻿using Apps.Monday.Api;
+using Apps.Monday.Constants;
+using Apps.Monday.Invocables;
+using Apps.Monday.Models.Dtos;
 using Apps.Monday.Models.Responses.Items;
 using Apps.Monday.Webhooks.Handlers.Items;
 using Apps.Monday.Webhooks.Models;
+using Apps.Monday.Webhooks.Models.Payloads;
+using Apps.Monday.Webhooks.Models.Responses;
+using Blackbird.Applications.Sdk.Common.Exceptions;
+using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Newtonsoft.Json;
 
 namespace Apps.Monday.Webhooks;
 
 [WebhookList]
-public class ItemWebhookList
+public class ItemWebhookList(InvocationContext invocationContext) : AppInvocable(invocationContext)
 {
     [Webhook("On item created", typeof(ItemCreatedHandler), 
         Description = "This event is triggered when item is created.")]
-    public Task<WebhookResponse<ItemResponse>> OnItemCreated(WebhookRequest request,
-        [WebhookParameter] BoardIdentifier boardIdentifier) => HandleWebhookRequest(request);
+    public Task<WebhookResponse<ItemResponse>> OnItemCreated(WebhookRequest request) 
+        => HandleWebhookRequest(request);
+
+    [Webhook("On item archived", typeof(ItemArchivedHandler),
+        Description = "This event is triggered when an item is archived.")]
+    public Task<WebhookResponse<ItemIdResponse>> OnItemArchived(WebhookRequest request)
+        => HandleArchivedOrDeletedRequest(request);
+
+    [Webhook("On item deleted", typeof(ItemDeletedHandler),
+        Description = "This event is triggered when an item is deleted.")]
+    public Task<WebhookResponse<ItemIdResponse>> OnItemDeleted(WebhookRequest request)
+        => HandleArchivedOrDeletedRequest(request);
     
-    private Task<WebhookResponse<ItemResponse>> HandleWebhookRequest(WebhookRequest request)
+    private async Task<WebhookResponse<ItemResponse>> HandleWebhookRequest(WebhookRequest request)
     {
         var body = request.Body.ToString()!;
         var challenge = JsonConvert.DeserializeObject<ChallengeDto>(body);
@@ -24,20 +41,63 @@ public class ItemWebhookList
         {
             var response = new HttpResponseMessage();
             response.Content = new StringContent(body);
-            return Task.Run(() => new WebhookResponse<ItemResponse>
+            return new WebhookResponse<ItemResponse>
+            {
+                ReceivedWebhookRequestType = WebhookRequestType.Preflight,
+                HttpResponseMessage = response
+            };
+        }
+
+        var itemPayload = JsonConvert.DeserializeObject<ItemPayload>(body, JsonConfig.JsonSettings)!;
+        var item = await GetItem(itemPayload.PulseId);
+        return new WebhookResponse<ItemResponse>
+        {
+            ReceivedWebhookRequestType = WebhookRequestType.Default,
+            Result = item
+        };
+    }
+    
+    private Task<WebhookResponse<ItemIdResponse>> HandleArchivedOrDeletedRequest(WebhookRequest request)
+    {
+        var body = request.Body.ToString()!;
+        var challenge = JsonConvert.DeserializeObject<ChallengeDto>(body);
+
+        if (challenge != null && !string.IsNullOrEmpty(challenge.Challenge))
+        {
+            var response = new HttpResponseMessage
+            {
+                Content = new StringContent(body)
+            };
+
+            return Task.FromResult(new WebhookResponse<ItemIdResponse>
             {
                 ReceivedWebhookRequestType = WebhookRequestType.Preflight,
                 HttpResponseMessage = response
             });
         }
-        
-        return Task.Run(() => new WebhookResponse<ItemResponse>
+
+        var itemPayload = JsonConvert.DeserializeObject<ItemPayload>(body, JsonConfig.JsonSettings)!;
+        return Task.FromResult(new WebhookResponse<ItemIdResponse>
         {
             ReceivedWebhookRequestType = WebhookRequestType.Default,
             Result = new()
             {
-                Id = body // TEMP
+                ItemId = itemPayload.PulseId
             }
         });
+    }
+
+    private async Task<ItemResponse> GetItem(string itemId)
+    {
+        var variables = new { ids = int.Parse(itemId) };
+        var request = new ApiRequest(GraphQlQueries.GetItemById, variables, Creds);
+
+        var response = await Client.ExecuteWithErrorHandling<DataWrapperDto<SearchItemsResponse>>(request);
+        if (response?.Data == null || !response.Data.Items.Any())
+        {
+            throw new PluginApplicationException($"Couldn't find board by specified ID ({itemId})");
+        }
+
+        return response.Data.Items.First();
     }
 }
---
+++ Apps.Monday/Webhooks/Models/Responses/ItemIdResponse.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.Monday.Webhooks.Models.Responses;
+
+public class ItemIdResponse
+{
+    [Display("Item ID")]
+    public string ItemId { get; set; } = string.Empty;
+}
