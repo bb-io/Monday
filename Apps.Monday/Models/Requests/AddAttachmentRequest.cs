366d8c0ae70336658a0af0bf512d1a44693bbd48
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2025-03-12 12:27:48 +0200

    Enhance attachment handling for updates; Refactor AddAttachmentAsync to create an update if none exists; Update AddAttachmentRequest to include optional UpdateId property; Improve unit tests for attachment functionality.

--- Apps.Monday/Models/Requests/AddAttachmentRequest.cs
+++ Apps.Monday/Models/Requests/AddAttachmentRequest.cs
@@ -1,11 +1,16 @@
-﻿using Apps.Monday.Models.Identifiers;
+﻿using Apps.Monday.DataSourceHandlers;
+using Apps.Monday.Models.Identifiers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Monday.Models.Requests;
 
-public class AddAttachmentRequest : UpdateIdentifier
+public class AddAttachmentRequest : ItemIdentifier
 {
+    [Display("Update ID"), DataSource(typeof(UpdateDataSource))]
+    public string? UpdateId { get; set; }
+
     [Display("File")]
     public FileReference File { get; set; } = default!;
 }
