96e7a827f5d8a0fb5bd552df428714176077b2f9
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-22 09:27:14 +0200

    add TestBase.PrintResult(), fix FileManager file path construction

--- Tests.Monday/Base/TestBase.cs
+++ Tests.Monday/Base/TestBase.cs
@@ -1,6 +1,7 @@
 ﻿using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Tests.Monday.Base;
 
@@ -40,4 +41,9 @@ public class TestBase
     public InvocationContext InvocationContext { get; set; }
 
     public FileManager FileManager { get; set; }
+
+    protected static void PrintResult(object result)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+    }
 }
