49199542086d47cbaa441195e1a3dd3f0a0aa4fa
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-23 11:15:14 +0200

    validate item id in ItemFileAssetDataHandler

--- Apps.Monday/DataSourceHandlers/ItemFileAssetDataHandler.cs
+++ Apps.Monday/DataSourceHandlers/ItemFileAssetDataHandler.cs
@@ -6,17 +6,31 @@ using Apps.Monday.Models.Identifiers;
 using Apps.Monday.Models.Responses.File;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Monday.DataSourceHandlers;
 
-public class ItemFileAssetDataHandler(
-    InvocationContext invocationContext,
-    [ActionParameter] ItemIdentifier itemIdentifier) : AppInvocable(invocationContext), IAsyncDataSourceItemHandler
+public class ItemFileAssetDataHandler : AppInvocable, IAsyncDataSourceItemHandler
 {
+    private readonly long _itemId;
+
+    public ItemFileAssetDataHandler(
+        InvocationContext invocationContext,
+        [ActionParameter] ItemIdentifier itemIdentifier) : base(invocationContext)
+    {
+        if (string.IsNullOrWhiteSpace(itemIdentifier.ItemId))
+            throw new PluginMisconfigurationException("Please specify an Item ID first");
+
+        if (!long.TryParse(itemIdentifier.ItemId, out long itemId))
+            throw new PluginMisconfigurationException("Item ID should be a number");
+        
+        _itemId = itemId;
+    }
+
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken ct)
     {
-        var variables = new { ids = long.Parse(itemIdentifier.ItemId) };
+        var variables = new { ids = _itemId };
         var request = new ApiRequest(GraphQlQueries.GetItemAssetsById, variables, Creds);
         
         var response = await Client.ExecuteWithErrorHandling<DataWrapperDto<SearchItemFilesResponse>>(request);
